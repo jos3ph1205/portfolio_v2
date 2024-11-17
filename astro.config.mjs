d7a46c4814046afce2067295dfe785c982165660
jos3ph1205 <sharvinjoseph.e@gmail.com>
2024-11-17 21:56:29 +0800

    Initialization

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+// https://astro.build/config
+export default defineConfig({
+   site: "https://sje.pages.dev/",
+   integrations: [sitemap(), tailwind()],
+});
